a4d08ef8d807ed2e93d3cffc4c87cc02ae35e927
Xinyu <shguan10@gmail.com>
2018-01-05 17:06:50 -0600

    initial commit

---
+++ ukkonen.c
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include "ukkonen.h"
+#include <assert.h>
+//#define NDEBUG //if included, removes assert statements
+#include <stdlib.h>
+
+size_t char2index(char c, size_t UNQ_CHARS){
+  return c;//TODO this needs to be adapted so that everything fits in UNQ_CHARS
+};
+
+inline void insertCurrSuffix(point *ap, size_t currSuffix){
+          //insert currSuffix at new branch at activePoint 
+        //  if prevNode is not NULL, then set prevNode->link = new node at activePoint
+        //  set prevNode = currNode
+}
+
+size_t ukkonen(char *instr, size_t instrlen, size_t UNQ_CHARS){//instrlen excludes terminating char
+  node *root = calloc(sizeof(node));
+  root->edges = calloc(UNQ_CHARS,sizeof(edge *));
+
+  point *ap=malloc(sizeof(point));
+  ap->node = root;
+  ap->depth=0;
+  ap->edge=NULL;//will always be NULL if depth == nodeDepth
+  
+  size_t currSuffix = 0;
+  
+  node *prevNewNode = NULL;
+  
+  for(size_t END = 1; END < instrlen; END++){
+    assert(currSuffix + ap->depth + 1 == END);
+    char currChar = instr[END-1];
+    currSuffix_loop://loops until currSuffix==END or currChar matches
+    //first decide if ap is at a node or on a branch, then check if currChar matches
+    if(ap->edge==NULL?
+      (ap->node->edges)[char2index(currChar,UNQ_CHARS)]==NULL :
+      instr[ap->edge->start + (ap->depth - ap->node->nodeDepth)]!=currChar ){
+      //char does not match, so we branch
+      assert( currSuffix < END );
+      //insertCurrSuffix(ap,currSuffix);
+      currSuffix++;
+      ap->node = ap->node->link;
+      if(ap->node==NULL)ap->node=root;
+      ap->depth--;
+      //calculate where this depth is
+      if(ap->depth==ap->node->nodeDepth){
+        //ap will be at the node, so we don't have to do anything
+      }else{
+        assert(ap->depth > ap->node->nodeDepth);//ap must go deeper than the node
+        hop_loop:
+        //calculate which branch to go
+        char nextchar = instr[ap->node->nodeDepth+currSuffix];
+        ap->edge = ap->node->edges[char2index(nextchar,UNQ_CHARS)];
+        size_t depthToGo = ap->depth - ap->node->nodeDepth;
+        
+        assert(ap->edge!=NULL);//this because depthToGo > 0
+        if(ap->edge->leaf==NULL?
+          depthToGo > END - ap->edge->start:
+          depthToGo > ap->edge->end - ap->edge->start){
+          
+        }
+      }
+      //activePoint is now positioned where it needs to be
+      //  so that currChar is the next to be considered
+      if(currSuffix<END) goto currSuffix_loop;
+      prevNewNode = NULL;//because it is root
+      //(assert currSuffix==END && activePoint is root && prevNode is NULL)
+    }else{
+      //advance activePoint along matching char
+    }
+    
+  }
+  //assert END == instrlen
+  //we now need to add the dollar sign
+  //then sum up the substrings, and subtract one to exclude the empty string
+  
+}
---
+++ ukkonen.h
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ukkonen.h
+ * Author: Xinyu
+ *
+ * Created on January 4, 2018, 1:20 PM
+ */
+
+#ifndef UKKONEN_H
+#define UKKONEN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+  //const size_t NUM_CHARS;
+  
+  typedef struct edge_header edge;
+  typedef struct node_header node;
+  typedef struct point_header point;
+  
+  struct edge_header{
+    node *leaf;//NULL if does not branch at end
+    size_t start;//inclusive
+    size_t end;//exclusive
+                //starts out as the actual instr end, including terminating char
+  };
+  
+  struct node_header{
+    edge **edges;//static array of NUM_CHARS children; NULL if does not have edge at a char
+    node *link; //points to node that has same address, except for the first char
+                //defaults to NULL, which means go to root after branching from here
+    size_t nodeDepth;
+    size_t numsubsbelow;
+  };
+  
+  struct point_header{
+    node *node;//all chars up to this branch point match currSuffix
+    edge *edge;
+    size_t depth;//represents total matching chars
+      //depth - node->nodeDepth will be the edge depth
+  };
+  
+  size_t ukkonen(char *instr, size_t instrlen, size_t UNQ_CHARS);//instrlen excludes terminating char
+  /*
+High-level description of Ukkonen's algorithm:
+We are basically inserting suffixes into the tree from longest to shortest.
+Tree representation:
+  Nodes (represent places where branch off), each containing:
+    collection of edges
+    link points to node that contains same path from root, except first char, defaults to NULL
+    number of substrings in sub and below
+  Edges (represent branches), each containing:
+    parent node?
+    child node
+    start and end indeces
+set activePoint to root, depth 0
+set root->link to NULL
+set currSuffix to suffix 0
+set prevNode to NULL
+Set END to 1
+Iterate END across the instr//don't include terminating char
+  (assert currSuffix < END)
+  currSuffix_loop://loops until currSuffix==END or currChar matches
+  if currSuffix has next char that does not match activePoint's children:
+    (meaning that curr char, which is always one b4 END, does not match)
+    insert currSuffix at new branch at activePoint 
+      if prevNode is not NULL, then set prevNode->link = new node at activePoint
+      set prevNode = activePoint->node
+    advance currSuffix 
+    follow activePoint->node->link to find next activePoint
+      if NULL, then set activePoint to root
+      otherwise set activePoint->node = activePoint->node->link
+      go to a depth-1, then calculate where that is on active edge 
+        may entail jumps
+        if currSuffix<END: goto currSuffix_loop
+    set prevNode to NULL (because it is root)
+    (assert currSuffix==END && activePoint is root && prevNode is NULL)
+  else: advance activePoint along sub that matches char
+    this will always increase depth
+  //assert currSuffix == END || currChar matches
+//assert currSuffix == END == actual instr end (without terminating char)
+ */
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UKKONEN_H */
+
